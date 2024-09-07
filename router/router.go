fe697527ed008334ac12b10d61c4b137eb5b2c23
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 20:58:06 -0300

    refactor: Add MalformedBodyMiddleware to router initialization

--- router/router.go
+++ router/router.go
@@ -1,11 +1,16 @@
 package router
 
 import (
+	"github.com/DiogoJorge1401/golang-api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeRouter() {
 	var router = gin.Default()
+
+	router.Use(middleware.MalformedBodyMiddleware)
+
 	initializeRoutes(router)
+
 	router.Run(":8080")
 }
