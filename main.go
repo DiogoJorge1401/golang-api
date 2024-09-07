3d144f6d636c8a6ef5a902169195c9f128dac027
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 12:02:42 -0300

    refactor: remove unnecessary code in main.go

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"github.com/DiogoJorge1401/golang-api/config"
 	"github.com/DiogoJorge1401/golang-api/router"
 )
 
 func main() {
-	logger := config.GetLoger("main")
-
-	if err := config.Init(); err != nil {
-		logger.Errorf("config initialization error: %v", err)
-		return
-	}
 
 	router.InitializeRouter()
 }
