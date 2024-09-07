2e9c9d23d3bb368cc6786b0be68de70d3d32567c
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-06 22:48:13 -0300

    feat: add OppeningResponse struct to schemas

--- schemas/oppenning.go
+++ schemas/oppenning.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -12,4 +14,15 @@ type Oppening struct {
 	Remote   bool
 	Link     string
 	Salary   int
+}
+type OppeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int       `json:"salary"`
 }
