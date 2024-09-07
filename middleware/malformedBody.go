a222d16dc95e33475a2934e9d2ab868a3e81f3a7
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 20:57:49 -0300

    feat: Add MalformedBodyMiddleware to handle malformed request bodies

---
+++ middleware/malformedBody.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/DiogoJorge1401/golang-api/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func MalformedBodyMiddleware(c *gin.Context) {
+	bodyBytes, err := c.GetRawData()
+	if err != nil {
+		handler.SendErrorJSONResponse(c, http.StatusBadRequest, "Unable to read request body")
+		c.Abort()
+		return
+	}
+
+	var body map[string]interface{}
+	if err := json.Unmarshal(bodyBytes, &body); err != nil {
+		handler.SendErrorJSONResponse(c, http.StatusBadRequest, fmt.Sprintf("Malformed request body: %v", err.Error()))
+		c.Abort()
+		return
+	}
+
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	c.Next()
+}
