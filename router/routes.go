554523fb90b507461c2d05cb5f793b907ec927e7
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 12:02:33 -0300

    refactor: update routes.go to initialize handler package and fix typo in function name

--- router/routes.go
+++ router/routes.go
@@ -1,11 +1,13 @@
 package router
 
 import (
+	"github.com/DiogoJorge1401/golang-api/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
+	handler.Init()
 
-	InitializeOppeningRoutes(v1)
+	InitializeOpeningRoutes(v1)
 }
