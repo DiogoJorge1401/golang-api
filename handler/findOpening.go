b480231c898219bea185bded9809a614d3de4ac3
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 21:21:28 -0300

    feat: get opening by id in FindOpeningHandler

--- handler/findOpening.go
+++ handler/findOpening.go
@@ -1,11 +1,22 @@
 package handler
 
 import (
-	"net/http"
+	"errors"
+	"fmt"
 
+	"github.com/DiogoJorge1401/golang-api/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func FindOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "GET Opening id"})
+	opening := &schemas.Opening{}
+	id := ctx.Param("id")
+
+	if results := db.First(opening, id); errors.Is(results.Error, gorm.ErrRecordNotFound) {
+		SendErrorJSONResponse(ctx, 404, fmt.Sprintf("Opening not found with id: %v", id))
+		return
+	}
+
+	SendJSONResponse(ctx, 200, gin.H{"data": opening})
 }
