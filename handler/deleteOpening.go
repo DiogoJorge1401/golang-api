56e0f3a8034bc4421f655202f2cb0f2b413d3ced
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-07 21:17:17 -0300

    feat: Delete opening by ID in DeleteOpeningHandler

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,11 +1,26 @@
 package handler
 
 import (
-	"net/http"
+	"errors"
+	"fmt"
 
+	"github.com/DiogoJorge1401/golang-api/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "DELETE Opening"})
+	opening := &schemas.Opening{}
+
+	id := ctx.Param("id")
+
+	if result := db.First(opening, id); errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		SendErrorJSONResponse(ctx, 404, fmt.Sprintf("Opening not found with id: %v", id))
+		return
+	}
+
+	db.Delete(opening)
+
+	SendJSONResponse(ctx, 204, gin.H{})
+
 }
