f901105898d54e30af0f270451a12147e55d0fe0
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-09-20 23:31:10 -0300

    feat: add Swagger documentation support to the router

--- router/router.go
+++ router/router.go
@@ -3,14 +3,24 @@ package router
 import (
 	"github.com/DiogoJorge1401/golang-api/middleware"
 	"github.com/gin-gonic/gin"
+
+	docs "github.com/DiogoJorge1401/golang-api/docs"
+
+	swaggerfiles "github.com/swaggo/files"
+
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitializeRouter() {
 	var router = gin.Default()
 
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
 	router.Use(middleware.MalformedBodyMiddleware)
 
 	initializeRoutes(router)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	router.Run(":8080")
 }
